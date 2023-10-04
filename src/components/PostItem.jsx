9624bf7d8a0b6c887dca635dec4b33ea33b03ccd
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-10-04 12:52:23 +0200

    avatar added

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,6 +1,10 @@
+import UserAvatar from "./UserAvatar";
+
 export default function PostItem({ post }) {
+    console.log(post);
     return (
         <article key={post.id}>
+            <UserAvatar uid={post.uid} />
             <img src={post.image} alt={post.caption} />
             <h3>{post.caption}</h3>
         </article>
