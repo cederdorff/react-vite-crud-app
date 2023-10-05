add7d13e3c24f4f53b2f2e2854ffd4c5d1432965
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-10-05 13:36:59 +0200

    Create, update and delete

--- src/pages/CreatePage.jsx
+++ src/pages/CreatePage.jsx
@@ -1,7 +1,56 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function CreatePage() {
+    const [caption, setCaption] = useState("");
+    const [image, setImage] = useState("");
+    const navigate = useNavigate();
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        const newPost = {
+            caption: caption,
+            image: image,
+            uid: "ZfPTVEMQKf9vhNiUh0bj"
+        };
+
+        const url =
+            "https://fb-rest-race-default-rtdb.firebaseio.com/posts.json";
+
+        const response = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify(newPost)
+        });
+
+        if (response.ok) {
+            navigate("/");
+        } else {
+            console.log("Something went wrong");
+        }
+    }
+
     return (
         <section className="page">
             <h1>Create New Post</h1>
+            <form onSubmit={handleSubmit}>
+                <label>Caption</label>
+                <input
+                    type="text"
+                    placeholder="Type a caption"
+                    value={caption}
+                    required
+                    onChange={event => setCaption(event.target.value)}
+                />
+                <label>Image</label>
+                <input
+                    type="url"
+                    placeholder="Paste an image url"
+                    value={image}
+                    required
+                    onChange={event => setImage(event.target.value)}
+                />
+                <button>Create</button>
+            </form>
         </section>
     );
 }
--- src/pages/UpdatePage.jsx
+++ src/pages/UpdatePage.jsx
@@ -1,7 +1,90 @@
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+
 export default function UpdatePage() {
+    const [post, setPost] = useState({});
+    const [caption, setCaption] = useState("");
+    const [image, setImage] = useState("");
+    const params = useParams();
+    const navigate = useNavigate();
+    const url = `
+    https://fb-rest-race-default-rtdb.firebaseio.com/posts/${params.postId}.json
+    `;
+
+    useEffect(() => {
+        async function getPost() {
+            const response = await fetch(url);
+            const data = await response.json();
+            console.log(data);
+            setPost(data);
+            setCaption(post.caption);
+            setImage(post.image);
+        }
+
+        getPost();
+    }, [post.caption, post.image, url]);
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        const postToUpdate = {
+            caption: caption,
+            image: image,
+            uid: post.uid
+        };
+
+        const response = await fetch(url, {
+            method: "PUT",
+            body: JSON.stringify(postToUpdate)
+        });
+
+        if (response.ok) {
+            navigate("/");
+        } else {
+            console.log("Something went wrong");
+        }
+    }
+
+    async function handleDelete() {
+        const wantToDelete = confirm("Are you sure you want to delete?");
+
+        if (wantToDelete) {
+            const response = await fetch(url, {
+                method: "DELETE"
+            });
+
+            if (response.ok) {
+                navigate("/");
+            } else {
+                console.log("Something went wrong");
+            }
+        }
+    }
+
     return (
         <section className="page">
             <h1>Update Page</h1>
+            <form onSubmit={handleSubmit}>
+                <label>Caption</label>
+                <input
+                    type="text"
+                    placeholder="Type a caption"
+                    value={caption}
+                    required
+                    onChange={event => setCaption(event.target.value)}
+                />
+                <label>Image</label>
+                <input
+                    type="url"
+                    placeholder="Paste an image url"
+                    value={image}
+                    required
+                    onChange={event => setImage(event.target.value)}
+                />
+                <img className="image-preview" src={image} alt="Choose" />
+
+                <button>Save</button>
+            </form>
+            <button onClick={handleDelete}>Delete</button>
         </section>
     );
 }
